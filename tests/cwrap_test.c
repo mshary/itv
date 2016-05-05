833583a7da271bc6b1bcbde7cb437532b5bdcb96
Muhammad Shahzad Shafi <shaheryarkh@gmail.com>
2016-05-05 23:38:09 +0200

    fixed test apps to better display itv info

--- tests/cwrap_test.c
+++ tests/cwrap_test.c
@@ -43,7 +43,7 @@ int main() {
 	sender_checksum = itv_characters_checksum(itv);
 
 	itv_characters_encode(itv, msg, enc, sizeof(enc));
-	printf("MSG: %s\nENC: %s\nCS: %lu\n", msg, enc, sender_checksum);
+	printf("MSG: %s\nENC: %s\nCS: %zx\n", msg, enc, sender_checksum);
 
 	free(itv);
 	itv = NULL;
@@ -53,9 +53,9 @@ int main() {
 
 	if (sender_checksum == receiver_checksum) {
 		itv_characters_decode(itv, enc, dec, sizeof(dec));
-		printf("DEC: %s\nRandom: %lu\nCS: %lu\n", dec, (unsigned long)get_random(len), receiver_checksum);
+		printf("DEC: %s\nRandom: %zx\nCS: %zx\n", dec, get_random(len), receiver_checksum);
 	} else {
-		printf("Failure: checksum mismatch, %lu != %lu", sender_checksum, receiver_checksum);
+		printf("Failure: checksum mismatch, %zx != %zx", sender_checksum, receiver_checksum);
 	};
 
 	free(itv);
